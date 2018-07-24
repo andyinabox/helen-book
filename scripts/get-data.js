bbe3858c238ddb28a6d213c2c96aab50c913731b
amdayton <amdayton@gmail.com>
2018-07-24 17:39:03 -0500

    Fix data fetching to work with auth

--- scripts/get-data.js
+++ scripts/get-data.js
@@ -1,26 +1,46 @@
 // usage:
 // node scripts/get-data.js data_doc_name [args]
 
+require('dotenv').config();
 const fs = require('fs');
 const querystring = require('querystring');
+const got = require('got');
+// const download = require('download');
+const Promise = require('es6-promise').Promise;
 
-require('dotenv').config();
 var argv = require('minimist')(process.argv.slice(2));
 var doc = argv._.length ? argv._[0] : 'entries';
 delete argv._;
 
-const download = require('download');
-const Promise = require('es6-promise').Promise;
+const gotOptions = {
+	baseUrl: process.env.COUCHDB_SERVER,
+	json: true,
+	body: JSON.stringify({
+		"name": process.env.COUCHDB_USER,
+		"password": process.env.COUCHDB_PASSWORD
+	})
+};
 
-var db = process.env.COUCHDB_SERVER;
+// these are loaded from .env file
+// you may also want to set `NODE_TLS_REJECT_UNAUTHORIZED=0`
+// if using a self-signed certificate
+const db = process.env.COUCHDB_SERVER;
+const constantsUrl = db + 'helendb/_design/main/_view/constants';
+const dataUrl = db + 'helendb/_design/main/_view/' + doc + '?' + querystring.stringify(argv);
+// in .env
 
-var constantsUrl = db + 'helendb/_design/main/_view/constants';
-var dataUrl = db + 'helendb/_design/main/_view/' + doc + '?' + querystring.stringify(argv);
+// var constants = download(constantsUrl, 'data/constants.json', options);
+// console.log('grabbing: '+dataUrl)
+// var data = download(dataUrl, 'data/constants.json', options);
 
 console.log('grabbing: '+constantsUrl)
-var constants = download(constantsUrl).pipe(fs.createWriteStream('data/constants.json'));
-console.log('grabbing: '+dataUrl)
-var data = download(dataUrl).pipe(fs.createWriteStream('data/data.json'));
+const constants = got.get(constantsUrl).then(response => {
+	fs.writeFileSync('data/constants.json', response.body);
+});
+console.log('grabbing: ' + dataUrl)
+const data = got.get(dataUrl).then(response => {
+	fs.writeFileSync('data/data.json', response.body);
+});
 
 Promise.all([constants, data], values => {
 	console.log(values);
